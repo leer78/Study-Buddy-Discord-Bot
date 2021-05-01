6a0e0027eb4fc3f5db0ca1aa412923dc755e6fbc
Wags15 <58407467+Wags15@users.noreply.github.com>
2021-04-30 21:48:51 -0400

    timer

--- Python/bot.py
+++ Python/bot.py
@@ -1,32 +1,38 @@
 import discord
+import time
+import datetime
+
 
 client = discord.Client()
 
 
 @client.event
-async def on_ready():
+async def on_ready():   
     print(client.user , " Has Logged In")
 
 
 @client.event
-async def on_message(message):
+async def on_message(message):    
     msg = message.content
+    msg_content = msg.split()
 
     if msg.startswith("+add "):
         schedule_add = open('Database\\{message.author}.txt'.format(message=message),"a")
         schedule_add.write(formatted_event(msg) + "\n")
         schedule_add.close()
         await message.channel.send("Event Has Been Added")
-
+        time.sleep(get_time(msg_content[2]))
+        await message.author.send("REMINDER: {event}".format(event = msg_content[1]))
     if msg.startswith("+read"):
         schedule_read = open('Database\\names.txt',"r")
 
 
 def formatted_event(msg):
     event = msg.split()
-    return event[1] + ": " + event[2] +"-" +event[3]
+    return event[1] + ": " + event[2] 
 
 
-    
 
-client.run('ODM3ODMxODk2MTQ2NzcxOTk4.YIyRwg.sFEwy8sipdy1_Sngyt48GaOx9rU')
+
+
+client.run('') # DELETE BEFORE PUSHING DISCORD DOES NOT LIKE WHEN WE SHARE TOKEN ONLINE
