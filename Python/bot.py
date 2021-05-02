4de0f778349521ea9919c372b054d09036086fe1
Wags15 <58407467+Wags15@users.noreply.github.com>
2021-05-01 20:22:40 -0400

    bot

--- Python/bot.py
+++ Python/bot.py
@@ -2,7 +2,6 @@ import asyncio
 import discord
 import time
 import datetime
-from staying_alive import staying_alive
 
 from events import *
 
@@ -20,46 +19,40 @@ async def on_ready():
 @client.event
 async def on_message(message: discord.message.Message):
     msg = message.content
-    start_message = "Invalid Command"
 
     # Adds eye strain command to the queue of events
     if (msg.startswith("+eyes")):
         queue.add(EyesCommand(datetime.datetime.now(),datetime.timedelta(), message.author))
-        start_message = "Eye Strain Reduction Session Has Begun! Type +eyes To End It."
 
     # Adds pomodor studying technique session to the queue of events (refer to website for )
-    if(msg.startswith("+pomodoro")):
-        #queue.add(PomodoroCommand(datetime.datetime.now(),datetime.timedelta(), message.author))
-        start_message = "Pomodoro Session Has Begun! Type +pomodoro To End It."
+    #if(msg.startswith("+pomodoro")):
+     #   queue.add(PomodoroCommand(datetime.datetime.now(),datetime.timedelta(), message.author))
 
-    if(msg.startswith("+gaming")):
-        #queue.add(GamingCommand(datetime.datetime.now(),datetime.timedelta(), message.author))
-        start_message = "Gaming Session Has Begun! Type +gaming To End It."
+    #if(msg.startswith("+gaming")):
+     #   queue.add(GamingCommand(datetime.datetime.now(),datetime.timedelta(), message.author))    
 
-    
-
-    await message.author.send(start_message)
-    await run_queue()
+    await tic_toc()
 
 
-
+async def tic_toc():    
+    if queue.is_ready():
+        await run_queue()
+    asyncio.sleep(30)
 
 async def run_queue():
-    while not queue.is_empty():
+    #while not queue.is_empty():
         # print("-")
-        if queue.is_ready():
-            event = queue.pop()
-            message = event.run_event(queue)
+       # if queue.is_ready():
+    event = queue.pop()
+    message = event.run_event(queue)
         # user = await client.fetch_user(event.user_id)
-            user = event.user_id
-            print(type(event))
-            if message is not None:
-                await user.send(message)
+    user = event.user_id
+    if message is not None:
+        await user.send(message)
 
 
 # Reads in the token
 with open('token.txt') as file:
     token = file.readline()
 
-staying_alive()
 client.run(token) # Launches the bot 
