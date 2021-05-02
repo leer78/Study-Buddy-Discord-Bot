233549a6dd9725a63a2280ef55113e91e56badff
Wags15 <58407467+Wags15@users.noreply.github.com>
2021-05-02 01:37:43 -0400

    updated website

--- Python/bot.py
+++ Python/bot.py
@@ -7,9 +7,7 @@ from discord.ext import tasks, commands
 from events import *
 
 
-
-
-bot = commands.Bot(command_prefix = '+')
+bot = commands.Bot(command_prefix='+')
 
 queue = EventQueue()
 
@@ -20,31 +18,39 @@ async def on_ready():
     print(bot.user, " Is Alive :)")
 
 
-@bot.command(name = "eyes")
+@bot.command(name="eyes")
 async def eyes(ctx):
     queue.add(EyesCommand(datetime.datetime.now(),datetime.timedelta(), ctx.author))
 
 
+@bot.command(name="pomodoro")
+async def pomodoro(ctx):
+    queue.add(PomodoroCommand(datetime.datetime.now(),datetime.timedelta(), ctx.author))
+
+
+# @bot.command(name="water")
+# async def water(ctx):
+#     queue.add(WaterCommand(datetime.datetime.now(),datetime.timedelta(), ctx.author))
+
+
 @tasks.loop(seconds=1)
-async def run_queue():   
+async def run_queue():
     if not queue.is_empty():
         if queue.is_ready():
             print(queue.view())
             event = queue.pop()
-            print("MADE IT BEFORE MESSAGE:" )
+            print("MADE IT BEFORE MESSAGE:")
             message = event.run_event(queue)
-            print("AFTER MESSAGE:" )
+            print("AFTER MESSAGE:")
             # user = await client.fetch_user(event.user_id)
             user = event.user_id
             if message is not None:
                 await user.send(message)
 
 
-
-
 run_queue.start()
 # Reads in the token
-with open('../token.txt') as file:
+with open('token.txt') as file:
     token = file.readline()
 
-bot.run(token) # Launches the bot 
+bot.run(token)  # Launches the bot
